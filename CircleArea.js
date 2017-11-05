3ca3ab626593dfab6cd4d682fc1c00057b3c46b1
raghuma4 <32066962+raghuma4@users.noreply.github.com>
2017-11-05 14:47:18 +0530

    Add files via upload

---
+++ CircleArea.js
@@ -0,0 +1,18 @@
+
+// finding the area of a cirlce by passing radius as arguemnt to the function
+function AreaOfCircle(radius) {
+	
+	var piValue = 3.14159;
+
+	var area = piValue * radius * radius;
+
+	// printing on the browser console
+	console.log("Area of circle with radius " +radius+ " is: " +area);
+}
+
+
+// Calling the function
+AreaOfCircle(5);
+
+// Calling the function
+AreaOfCircle(8);
